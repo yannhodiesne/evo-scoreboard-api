18ec7582d62ef1e779971e4a4adc93a02b21709c
Skordy <sk8ordiejohn@hotmail.com>
2021-08-24 14:14:34 -0700

    Updated Stats Model

--- app/models/stats.model.js
+++ app/models/stats.model.js
@@ -3,7 +3,7 @@ const sql = require("./db");
 const Stat = {};
 
 Stat.getAll = () => new Promise((resolve, reject) => {
-    sql.query(`SELECT * FROM server-stats`, (err, res) => {
+    sql.query(`SELECT * FROM "server-stats"`, (err, res) => {
         if (err) {
             console.log("Stat.getAll : ", err);
             reject(err);
